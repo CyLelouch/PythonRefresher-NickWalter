ec2eabcf9c33c97fe546e06ce9508128f3bbb5ae
CyLelouch <forgithub.cy@gmail.com>
2022-10-06 13:47:35 -0400

    first commit

---
+++ Python1.py
@@ -0,0 +1,45 @@
+#This is a comment
+
+print ("This is a string")
+ 
+var123 = ("this is a var")
+
+const = ("This is a const")
+print("....................")
+print("are you bored???")
+print("....................")
+print("answer y or n")
+BoredOfThis = input("y or n>>")
+killMe = "you have insta--Death, congrats \n you can go home now"
+if BoredOfThis == "y":
+    print(killMe)
+else:
+    print("you sure?")
+
+def hello():
+    print("This is a function")
+    print("....................")
+    print("are you bored???")
+    print("....................")
+    print("answer y or n")
+    BoredOfThis = input("y or n>>")
+    killMe = "you have insta--Death, congrats \n you can go home now"
+    if BoredOfThis == "y":
+        print(killMe)
+        a = input(" you may go now. \n hit enter to restart")
+        print(a)
+    else:
+        print("you sure?")
+while True:
+    hello()
+
+#Now im creating a list
+dognames = ["fido", "snoopDog","ChineseFood-...mmmmm yum"]
+print(dognames)
+#del(dognames[2]) no more chinese food. D: also len for length. 
+
+print("thats all im doing review wise. fuck classes, next ")
+
+
+
+
